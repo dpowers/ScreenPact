a9b5850a202781cfe391ca98b15453a502818577
David Powers <david@grayskies.net>
2021-11-29 09:10:30 -0500

    Initial Commit

---
+++ ScreenPact/ScreenPactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenPactApp.swift
+//  ScreenPact
+//
+//  Created by David Powers on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ScreenPactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
